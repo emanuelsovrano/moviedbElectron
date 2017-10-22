5c653db863db33ca024959d90ff757676fbac534
emanuel.sovrano <emanuel.sovrano@outlook.com>
2017-10-22 15:34:26 +0200

    added eslint , set rules

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
-let win
+let win;
 
 function createWindow() {
     //create a window object
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
     
     //load window object
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
